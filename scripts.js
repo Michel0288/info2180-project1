18b106242c12fe27cd8accd36bab7867cb94a53b
Michel Davies <micheldavies0288@gmail.com>
2020-11-01 20:47:46 -0500

    Added email subscription

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-/* Add your JavaScript to this file */
+onload=function(){
+    let email = document.querySelector('#email')
+    let message = document.querySelector('.message')
+    addEventListener('submit', (e) =>{
+        e.preventDefault()
+        if(!email.value == ''){
+            message.textContent = `Thank you! your email address ${email.value} has been added to our mailing list`
+            email.value = ''
+        }else{
+            message.textContent = 'Please enter a valid email address'
+            email.value = ''
+        }
+    })
+}
